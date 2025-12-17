72c237abbc597f1ed47f1c26f5ef8041a8f8390b
ranjith <ranjithkumark1130@gmail.com>
2025-12-17 19:49:12 +0530

    Add root route welcome message

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Neuroverse Galaxy Backend is Running!');
+});
+
 // --- ROUTES ---
 
 // Admin Login
